d729c3651659443ff91787e856580a7f37288f5c
oliver <oliver@sautinsoft.com>
2023-03-30 16:04:16 +0400

    Updated Code Examples

---
+++ CSharp/Make a password in resulting PDF/Sample.cs
@@ -0,0 +1,39 @@
+using System;
+using System.IO;
+using SautinSoft;
+
+namespace Sample
+{
+    class Sample
+    {
+        static void Main(string[] args)
+        {
+            // Make a password in resulting PDF
+            ExcelToPdf x = new ExcelToPdf();
+
+            // Set PDF as output format.
+            x.OutputFormat = SautinSoft.ExcelToPdf.eOutputFormat.Pdf;
+
+            // Set the password for PDF.
+            x.Options.PdfSecurity.EncryptionAlgorithm = ExcelToPdf.PdfEncryptionAlgorithm.RC4_128;
+            x.Options.PdfSecurity.Permissions = ExcelToPdf.PdfPermissions.Printing;
+            x.Options.PdfSecurity.UserPassword = "123456";
+            // The password cannot be in PDF-A. Change to PDF 1.5.
+            x.Options.PdfVersion = ExcelToPdf.COptions.ePdfVersion.PDF_15;
+
+            string excelFile = Path.GetFullPath(@"..\..\test.xlsx");
+            string pdfFile = Path.ChangeExtension(excelFile, ".pdf"); ;
+
+            try
+            {
+                x.ConvertFile(excelFile, pdfFile);
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(pdfFile) { UseShellExecute = true });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ VB.Net/Make a password in resulting PDF/Sample.vb
@@ -0,0 +1,32 @@
+Imports System
+Imports System.IO
+Imports SautinSoft
+
+Module Sample
+
+    Sub Main()
+        ' Make a password in resulting PDF
+        Dim x As New ExcelToPdf()
+
+        ' Set PDF as output format.
+        x.OutputFormat = SautinSoft.ExcelToPdf.eOutputFormat.Pdf
+
+        ' Set the password for PDF.
+        x.Options.PdfSecurity.EncryptionAlgorithm = ExcelToPdf.PdfEncryptionAlgorithm.RC4_128
+        x.Options.PdfSecurity.Permissions = ExcelToPdf.PdfPermissions.Printing
+        x.Options.PdfSecurity.UserPassword = "123456"
+        ' The password cannot be in PDF-A. Change to PDF 1.5.
+        x.Options.PdfVersion = ExcelToPdf.COptions.ePdfVersion.PDF_15
+
+        Dim excelFile As String = Path.GetFullPath("..\test.xlsx")
+        Dim pdfFile As String = Path.ChangeExtension(excelFile, ".pdf")
+
+        Try
+            x.ConvertFile(excelFile, pdfFile)
+            System.Diagnostics.Process.Start(New System.Diagnostics.ProcessStartInfo(pdfFile) With {.UseShellExecute = True})
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+            Console.ReadLine()
+        End Try
+    End Sub
+End Module
