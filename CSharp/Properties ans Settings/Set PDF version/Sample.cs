afc53de5891b2582ecabbe1a1e637d6a4ab9e162
oliver <oliver@sautinsoft.com>
2023-08-30 09:14:31 +0400

    New Code Samples

---
+++ CSharp/Properties ans Settings/Set PDF version/Sample.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using SautinSoft;
+
+namespace Sample
+{
+    class Sample
+    {
+        static void Main(string[] args)
+        {
+            // Convert Excel file to PDF file
+            ExcelToPdf x = new ExcelToPdf();
+
+            // Set PDF as output format.
+            x.OutputFormat = SautinSoft.ExcelToPdf.eOutputFormat.Pdf;
+            // Set pdf out version as PDF_A
+            // The component can create PDF_A1, PDF_A2, etc
+            x.Options.PdfVersion = ExcelToPdf.COptions.ePdfVersion.PDF_A1;
+
+            string excelFile = Path.GetFullPath(@"..\..\..\test.xlsx");
+            string pdfFile = Path.ChangeExtension(excelFile, ".pdf"); ;
+
+            try
+            {
+                x.ConvertFile(excelFile, pdfFile);
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(pdfFile) { UseShellExecute = true });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ VB.Net/Properties ans Settings/Set PDF version/Sample.vb
@@ -0,0 +1,30 @@
+Imports System
+Imports System.IO
+Imports SautinSoft
+
+Namespace Sample
+	Friend Class Sample
+		Shared Sub Main(ByVal args() As String)
+			' Convert Excel file to PDF file
+			Dim x As New ExcelToPdf()
+
+			' Set PDF as output format.
+			x.OutputFormat = SautinSoft.ExcelToPdf.eOutputFormat.Pdf
+
+			' Set pdf out version as PDF_A
+			' The component can create PDF_A1, PDF_A2, etc
+			x.Options.PdfVersion = ExcelToPdf.COptions.ePdfVersion.PDF_A1
+
+			Dim excelFile As String = Path.GetFullPath("..\..\..\test.xlsx")
+			Dim pdfFile As String = Path.ChangeExtension(excelFile, ".pdf")
+
+			Try
+				x.ConvertFile(excelFile, pdfFile)
+				System.Diagnostics.Process.Start(New System.Diagnostics.ProcessStartInfo(pdfFile) With {.UseShellExecute = True})
+			Catch ex As Exception
+				Console.WriteLine(ex.Message)
+				Console.ReadLine()
+			End Try
+		End Sub
+	End Class
+End Namespace
