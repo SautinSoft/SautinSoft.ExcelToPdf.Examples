288b59e404bb19614d7407947635cbaf74845dd7
developer@SupportPC <Developer@SupportPC>
2024-11-26 08:53:42 +0400

    New Code Samples

---
+++ CSharp/01. Excel to PDF/05. Convert a password protected workbook to PDF/Program.cs
@@ -0,0 +1,39 @@
+﻿using SautinSoft;
+using System;
+using System.IO;
+
+namespace Sample
+{
+    class Sample
+    {
+        static void Main(string[] args)
+        {
+            // Convert a password protected workbook
+            ExcelToPdf x = new ExcelToPdf();
+
+            // Set PDF as output format.
+            x.OutputFormat = SautinSoft.ExcelToPdf.eOutputFormat.Pdf;
+
+            // Set the user password for restricting access to opening the PDF file.
+            x.Options.PdfSecurity.UserPassword = "qwerty";
+
+            // Set the owner password for controlling permissions for editing, printing, and other modifications.
+            x.Options.PdfSecurity.OwnerPassword = "";
+
+            string excelFile = Path.GetFullPath(@"..\..\..\test.xlsx");
+            string pdfFile = Path.ChangeExtension(excelFile, ".pdf"); ;
+
+            try
+            {
+                x.ConvertFile(excelFile, pdfFile);
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(pdfFile) { UseShellExecute = true });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ VB.Net/01. Excel to PDF/05. Convert a password protected workbook to PDF/Program.vb
@@ -0,0 +1,34 @@
+﻿Imports SautinSoft
+Imports System
+Imports System.IO
+
+Namespace Sample
+	Friend Class Sample
+		Shared Sub Main(ByVal args() As String)
+			' Convert a password protected workbook
+			Dim x As New ExcelToPdf()
+
+			' Set PDF as output format.
+			x.OutputFormat = SautinSoft.ExcelToPdf.eOutputFormat.Pdf
+
+			' Set the user password for restricting access to opening the PDF file.
+			x.Options.PdfSecurity.UserPassword = "qwerty"
+
+			' Set the owner password for controlling permissions for editing, printing, and other modifications.
+			x.Options.PdfSecurity.OwnerPassword = ""
+
+			Dim excelFile As String = Path.GetFullPath("..\..\..\test.xlsx")
+			Dim pdfFile As String = Path.ChangeExtension(excelFile, ".pdf")
+
+
+			Try
+				x.ConvertFile(excelFile, pdfFile)
+				System.Diagnostics.Process.Start(New System.Diagnostics.ProcessStartInfo(pdfFile) With {.UseShellExecute = True})
+			Catch ex As Exception
+				Console.WriteLine(ex.Message)
+				Console.ReadLine()
+			End Try
+
+		End Sub
+	End Class
+End Namespace
